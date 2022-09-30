2c1babe4f107d879c9657c4ece3e2af478ac4138
JOMADELEMA <jomadelema@gmail.com>
2022-09-30 07:22:33 -0600

    code standard fix

--- src/instructions.js
+++ src/instructions.js
@@ -1,6 +1,6 @@
-export  const instructions = {
-    title: 'What is a Prime Number?',
-    body: `
+export const instructions = {
+  title: 'What is a Prime Number?',
+  body: `
     <p>According to wikipedia, a prime number is a natural number greater than 1 that is not a product of two smaller natural numbers. In summary, the following requirements must be met:  </p>
 
     <ul>
@@ -8,6 +8,4 @@ export  const instructions = {
     <li>Must be greater than 1</li>
     <li>Must not be a product of two smaller natural numbers</li>
     </ul>`
-  }
-
-
+}
--- src/main.js
+++ src/main.js
@@ -1,16 +1,16 @@
-import { primeNumber } from "./primeNumber.js";
-import { instructions } from "./instructions.js";
+import { primeNumber } from './primeNumber.js'
+import { instructions } from './instructions.js'
 
-const button = document.querySelector("#validate-button");
-const modalButton = document.querySelector("#modal-button");
-const input = document.querySelector("#text-input");
-const modal = document.querySelector("#modal");
-const modalContent = document.querySelector("#modal-content")
-const modalTitle = document.querySelector("#modal-title");
-const modalText = document.querySelector("#modal-text");
-const instructionsButton = document.querySelector("#button-instructions");
+const button = document.querySelector('#validate-button')
+const modalButton = document.querySelector('#modal-button')
+const input = document.querySelector('#text-input')
+const modal = document.querySelector('#modal')
+const modalContent = document.querySelector('#modal-content')
+const modalTitle = document.querySelector('#modal-title')
+const modalText = document.querySelector('#modal-text')
+const instructionsButton = document.querySelector('#button-instructions')
 
-const showInstructions = ()=>{
+const showInstructions = () => {
   modalTitle.innerText = instructions.title
   modalText.innerHTML = instructions.body
   toggleModal()
@@ -20,50 +20,49 @@ const showInstructions = ()=>{
   modalText.classList.add('modal-text-instructions')
 }
 
-
 const toggleModal = () => {
-  modal.classList.toggle("invisible");
-};
+  modal.classList.toggle('invisible')
+}
 const verifyPrime = () => {
-  modalTitle.innerText = "Result:";
+  modalTitle.innerText = 'Result:'
   modalContent.classList.remove('modal-bg-big')
   modalContent.classList.add('modal-bg')
   modalText.classList.remove('modal-text-instructions')
   modalText.classList.add('modal-text')
   try {
-    const result = primeNumber(input.value);
-    console.log(result);
+    const result = primeNumber(input.value)
+    console.log(result)
     if (result) {
-      modalText.innerText = "The number " + input.value + " is a prime number.";
-      toggleModal();
-      return;
+      modalText.innerText = 'The number ' + input.value + ' is a prime number.'
+      toggleModal()
+      return
     }
 
     modalText.innerText =
-      "The number " + input.value + " is not a prime number.";
-    toggleModal();
+      'The number ' + input.value + ' is not a prime number.'
+    toggleModal()
   } catch (err) {
-    modalText.innerText = err;
-    toggleModal();
+    modalText.innerText = err
+    toggleModal()
   }
-};
+}
 
-instructionsButton.addEventListener("click", showInstructions)
-button.addEventListener("click", verifyPrime);
-modalButton.addEventListener("click", toggleModal);
+instructionsButton.addEventListener('click', showInstructions)
+button.addEventListener('click', verifyPrime)
+modalButton.addEventListener('click', toggleModal)
 
-input.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    button.click();
+input.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    button.click()
   }
-});
+})
 
-modal.addEventListener("keypress", function (event) {
-  if (event.key === "Enter") {
-    event.preventDefault();
-    modalButton.click();
+modal.addEventListener('keypress', function (event) {
+  if (event.key === 'Enter') {
+    event.preventDefault()
+    modalButton.click()
   }
-});
+})
 
-showInstructions();
+showInstructions()
